da844bfb273937d231363c77db15dfdc93cc3739
Thinh Huynh <ThinhHuynh@Thinhs-MacBook-Pro.local>
2015-07-17 00:07:09 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -191,7 +191,7 @@
         // if the left corner is one complete width off the screen, move it to the right
         if (groundScreenPosition.x <= (-1 * ground.contentSize.width))
         {
-            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width + 100, ground.position.y);
+            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width + 100000, ground.position.y);
         }
     }
     
